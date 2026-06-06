b552a6151b0d3fc556c18267b2894ca309edc275
nobre-devl <nobre.gn@protonmail.com>
2026-06-06 17:34:45 -0300

    Conectando ao banco online

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://classico-automobilismo.onrender.com",
 });
 
 export const loginUser = async (email, password) => {
